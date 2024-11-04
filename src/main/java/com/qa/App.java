7aeccfe12e495a10fac645655f07b23cc6f0e4d6
Stamatis Theocharous <StamatisTheocharous@gmail.com>
2024-11-04 14:00:46 +0000

    meeeeessage

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -11,8 +11,8 @@ public class App
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Stamatis"));
+        System.out.println(sayHelloToSomeone("Stamatis"));
         System.out.println(sayGoodbye());
     }
 
